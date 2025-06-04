2bd54c51d2b12b93646cbed13f6752ce82df9c81
Ataman Sedghi <ataman.sedghi@hotmail.com>
2025-06-04 18:32:52 -0500

    Secure hello endpoint with JWT

--- src/test/java/com/example/demo/HelloControllerTests.java
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -14,8 +15,17 @@ class HelloControllerTests {
     private WebTestClient webTestClient;
 
     @Test
-    void helloEndpointReturnsHelloWorld() {
+    void helloEndpointRequiresAuthentication() {
         webTestClient.get()
+                .uri("/hello")
+                .exchange()
+                .expectStatus().isUnauthorized();
+    }
+
+    @Test
+    void helloEndpointReturnsHelloWorldWhenAuthenticated() {
+        webTestClient.mutateWith(SecurityMockServerConfigurers.mockJwt().authorities(() -> "SCOPE_demo.read"))
+                .get()
                 .uri("/hello")
                 .exchange()
                 .expectStatus().isOk()
