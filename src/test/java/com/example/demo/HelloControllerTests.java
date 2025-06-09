f55fb9f47eefe638f36ae60dc704053327c18293
Ataman Sedghi <ataman.sedghi@hotmail.com>
2025-06-09 18:16:15 -0500

    Map roles claim to authorities

--- src/test/java/com/example/demo/HelloControllerTests.java
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -24,7 +25,16 @@ class HelloControllerTests {
 
     @Test
     void helloEndpointReturnsHelloWorldWhenAuthenticated() {
-        webTestClient.mutateWith(SecurityMockServerConfigurers.mockJwt().authorities(() -> "SCOPE_demo.read"))
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        authoritiesConverter.setAuthoritiesClaimName("roles");
+        authoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        webTestClient.mutateWith(
+                SecurityMockServerConfigurers.mockJwt()
+                        .jwt(jwt -> jwt
+                                .claim("iss", "https://login.microsoftonline.com/{tenant-id}/v2.0")
+                                .claim("roles", "demo.read"))
+                        .authorities(authoritiesConverter))
                 .get()
                 .uri("/hello")
                 .exchange()
